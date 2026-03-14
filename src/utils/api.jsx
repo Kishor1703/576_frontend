23598caa45fef2b1774b886ae739aca502c02cce
Kishor1703 <pskishor196@gmail.com>
2026-03-14 16:34:21 +0530

    backend added

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: '/api' });
+const API = axios.create({ baseURL: 'https://576-backend.vercel.app/api' });
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('adminToken');
